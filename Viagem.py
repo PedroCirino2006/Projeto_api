ebe4bccf50e283b8042206f34eaffedef7fe44dc
PedroCirino2006 <pcp6221@gmail.com>
2024-04-29 15:10:45 -0300

    Pedindo nome para o usuário

--- Viagem.py
+++ Viagem.py
@@ -1,6 +1,7 @@
 distancia = float(input("Insira a distância do percurso (em km): "))
 velocidade = float(input("Insira a velocidade do veículo (em km/h): "))
+name = input("Qual o nome do motorista: ")
 
 tempo_viagem = distancia / velocidade
 
-print(f"O tempo de viagem será de aproximadamente {tempo_viagem:.2f} horas.")
+print(f"O tempo de viagem será de aproximadamente {tempo_viagem:.2f} horas. com o motorista {name}")
